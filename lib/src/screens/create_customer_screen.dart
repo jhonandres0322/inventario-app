1cfe2c842a713d9717923d392ccf4a18f3a89230
Jhon Andrés Parra Rodríguez <andresparrarodriguez@hotmail.com>
2025-07-27 20:49:10 -0500

    💄 Create add customer screen

--- lib/src/screens/create_customer_screen.dart
+++ lib/src/screens/create_customer_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:inventario_app/src/widgets/text_form_field_widget.dart';
 
 class CreateCustomerScreen extends StatelessWidget {
   const CreateCustomerScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(title: Text('Crear Cliente')),
+        body: Stack(children: [_MainFormCreateCustomer()]),
+      ),
+    );
+  }
+}
+
+class _MainFormCreateCustomer extends StatelessWidget {
+  const _MainFormCreateCustomer();
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Container(
+        margin: EdgeInsets.only(
+          top: size.height * 0.05,
+          left: size.width * 0.04,
+          right: size.width * 0.04,
+        ),
+        child: Form(
+          child: Column(
+            children: [
+              TextFormFieldWidget(
+                hintText: 'Ingrese el nombre del cliente',
+                labelText: 'Nombre',
+                validator: (value) => null,
+              ),
+              SizedBox(height: size.height * 0.03),
+              TextFormFieldWidget(
+                hintText: 'Ingrese el celular',
+                labelText: 'Celular',
+                validator: (value) => null,
+              ),
+              SizedBox(height: size.height * 0.03),
+              TextFormFieldWidget(
+                hintText: 'Ingrese la dirección',
+                labelText: 'Dirección',
+                validator: (value) => null,
+              ),
+              SizedBox(height: size.height * 0.08),
+              SizedBox(
+                width: size.width * 0.9,
+                child: ElevatedButton(onPressed: () {}, child: Text('Guardar')),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
